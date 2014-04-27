8ea161bf8f48615f6b911e87a96aae85a4e8f92b
Dražen Lučanin <kermit666@gmail.com>
2014-04-27 23:44:22 +0200

    Built qprc from commit 7e6c8da on branch master

---
+++ README.md
@@ -0,0 +1,60 @@
+qPRC
+====
+Web UI for visualising DNA concentration over time.
+
+Notebook
+--------
+While developing the workflow, we are using a Python notebook.
+Assuming you have your
+[Python sci dev environment set up](http://www.scipy.org/install.html),
+you can start it with:
+
+    cd notebook
+    ipython notebook --pylab=inline
+
+And see it in your browser at [http://127.0.0.1:8888/](http://127.0.0.1:8888/).
+
+If you just want to view the contents, you can see it on
+[nbviewer][notebook-nbviewer] without installing anything.
+
+[notebook-nbviewer]: http://nbviewer.ipython.org/urls/raw.githubusercontent.com/qPRC/qPRC/master/notebook/qPRC.ipynb "qPRC workflow notebook"
+
+Web app
+-------
+
+### Development
+
+Get ready:
+
+    mkvirtualenv -p /usr/bin/python3 qPRC
+    pip install -r requirements/dev.txt
+    npm install
+    bower install
+    grunt build
+
+Link up your front+backend:
+
+    util/bootstrap.sh
+
+Then in one tab spin up the frontend workflow:
+
+    grunt serve
+
+And in another the backend workflow:
+
+    ./manage.py runserver_plus
+
+
+### Production
+
+Everything the same, except:
+
+    pip install -r requirements/prod.txt
+    util/boostrap.sh prod
+
+Or use the prod brunch:
+
+    grunt publish
+    cd prod
+    git checkout prod
+    ./manage.py runserver
---
+++ qPRC/settings.py
@@ -0,0 +1,112 @@
+"""
+Django settings for qPRC project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'ux$tg!x3vmhf&autsr-3t1n*xz!h1ipacozha%i4+l+i))6^rt'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'qPRC.urls'
+
+WSGI_APPLICATION = 'qPRC.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+if 'DATABASE_URL' in os.environ: # production environment
+    # SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = True
+    TEMPLATE_DEBUG = True
+    # DB config
+    import dj_database_url
+    DATABASES['default'] =  dj_database_url.config()
+    # allow all host headers
+    ALLOWED_HOSTS = ['*']
+    # Honor the 'X-Forwarded-Proto' header for request.is_secure()
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+else: # development environment
+    DEBUG = True
+    TEMPLATE_DEBUG = True
+    # print e-mails to the console instead of sending them
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    INSTALLED_APPS = INSTALLED_APPS + ('django_extensions',)
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'Europe/Zagreb'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+# Templates
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates'),
+    os.path.join(BASE_DIR, '../static') # for the base Yeoman template
+)
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, '../static'),
+    os.path.join(BASE_DIR, '../.tmp') # during grunt serve
+)
