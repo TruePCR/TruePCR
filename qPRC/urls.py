3241ca731a65b48baf2a98e75f678df455ad6424
Dražen Lučanin <kermit666@gmail.com>
2014-04-29 00:19:43 +0200

    dashboard = landing page

--- qPRC/urls.py
+++ qPRC/urls.py
@@ -5,7 +5,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'qPRC.views.home', name='home'),
+    # url(r'^$', 'qPRC.views.home', name='home'),
+    url(r'^$', 'qPRC.views.dashboard', name='dashboard'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
