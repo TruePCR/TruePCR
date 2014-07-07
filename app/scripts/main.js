be42f2b343739b9ac6465dd8616ba9befd834ffd
Dražen Lučanin <kermit666@gmail.com>
2014-07-08 00:46:14 +0200

    dropzone.js drag'n'drop file upload

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,24 @@
 console.log('\'Allo \'Allo!');
+
+Dropzone.options.myDropzone = {
+
+    // Prevents Dropzone from uploading dropped files immediately
+    autoProcessQueue : false,
+
+    init : function() {
+        var submitButton = document.querySelector("#submit-all")
+        myDropzone = this;
+
+        submitButton.addEventListener("click", function() {
+            myDropzone.processQueue();
+            // Tell Dropzone to process all queued files.
+        });
+
+        // You might want to show the submit button only when
+        // files are dropped here:
+        this.on("addedfile", function() {
+            // Show submit button here and/or inform user to click it.
+        });
+
+    }
+};
--- qPRC/apps/datasets/views.py
+++ qPRC/apps/datasets/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 from django.template import RequestContext
+from django.core.urlresolvers import reverse
+from django.shortcuts import render_to_response
 
 from .forms import DatasetForm
 from .models import Dataset
@@ -11,9 +12,9 @@ def home(request):
         if form.is_valid():
             new_file = Dataset(file=request.FILES['file'])
             new_file.save()
-            return HttpResponseRedirect('datasets:home')
+            return HttpResponseRedirect(reverse('datasets:home'))
     else:
         form = DatasetForm
     data = {'form': form}
-    return render_to_response('dashboard.html', data,
+    return render_to_response('datasets/index.html', data,
                               context_instance=RequestContext(request))
