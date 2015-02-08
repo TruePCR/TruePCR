9650955dd630bdf1de6ed457f84f184278a3ced1
Dražen Lučanin <kermit666@gmail.com>
2015-02-08 22:05:40 +0100

    cleaner output

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -55,22 +55,3 @@ $(function() {
     };
 
 });
-
-
-// Rickshaw test
-var graph = new Rickshaw.Graph( {
-    element: document.querySelector("#chart"), 
-    width: 300, 
-    height: 200, 
-    series: [{
-        color: 'steelblue',
-        data: [ 
-            { x: 0, y: 40 }, 
-            { x: 1, y: 49 }, 
-            { x: 2, y: 38 }, 
-            { x: 3, y: 30 }, 
-            { x: 4, y: 32 } ]
-    }]
-});
- 
-graph.render();
--- app/scripts/plot_dataset.js
+++ app/scripts/plot_dataset.js
@@ -1,22 +1,25 @@
 // Plot dataset
 
  $(function(){
+     // default options
      var well = 15;
      var dye = 'SYBR';
 
      function plot_dataset(well, dye){
-         var id = 1;
+         var id = 1; // TODO: vary based on the opened page
          var url = '/' + id + '/well/' + well + '/dye/' + dye;
-         console.log(url);
          // plot graph
          $.getJSON(url, function(data){
-             console.log(data);
+             // format the data for Rickshaw
              var data2 = [];
              for(key in data){
                  data2.push({x: +key, y: data[key]});
              }
+             // clear old graph
+             $('#graph').empty();
+             // add new graph
              var graph = new Rickshaw.Graph( {
-                 element: document.querySelector("#chart-custom"),
+                 element: document.querySelector("#graph"),
                  width: 300,
                  height: 200,
                  series: [{
@@ -35,19 +38,18 @@
          });
      };
 
+     // initial plot
      plot_dataset(well, dye);
 
      // on well change redraw the graph
      $('#well-input').on("change", function(){
          well = $(this).val();
-         console.log($(this).val());
          plot_dataset(well, dye);
      });
 
      // on colour change redraw the graph
      $('#dye-input').on("change", function(){
          dye = $(this).val();
-         console.log($(this).val());
          plot_dataset(well, dye);
      });
 
