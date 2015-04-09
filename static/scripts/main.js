7b1cd8d07764cd9f5bfee977a7c575d28088c5a8
Dražen Lučanin <kermit666@gmail.com>
2015-04-09 16:08:18 +0200

    Built qprc from commit 49f8333 on branch master

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -65,6 +65,7 @@ $(function() {
      var dye = 'SYBR';
 
      function plot_dataset(well, dye){
+         // the main function that fetches and plots the data
          var id = 1; // TODO: vary based on the opened page
          var url = '/' + id + '/well/' + well + '/dye/' + dye;
          // plot graph
@@ -74,43 +75,51 @@ $(function() {
              for(key in data){
                  data2.push({x: +key, y: data[key]});
              }
-             // clear old graph
-             $('#graph').empty();
-             // add new graph
-             var graph = new Rickshaw.Graph( {
-                 element: document.querySelector("#graph"),
-                 width: 600,
-                 height: 400,
-                 series: [{
-                     color: 'steelblue',
-                     data: data2
-                     //   [
-                     //   { x: 0, y: 40 },
-                     //   { x: 1, y: 49 },
-                     //   { x: 2, y: 38 },
-                     //   { x: 3, y: 30 },
-                     //   { x: 4, y: 32 }
-                     // ]
-                 }]
-             });
-             // set the axes
-             var axes = new Rickshaw.Graph.Axis.X({
-                 graph: graph,
-                 orientation: 'top'
-             });
-
-             var y_axis = new Rickshaw.Graph.Axis.Y({
-                 graph: graph,
-                 orientation: 'left',
-                 tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
-                 element: document.getElementById('y_axis')
-             });
-             graph.render();
+             visualise(data2);
          });
      };
 
+     function visualise(data){
+         // clear old graph
+         $('#graph').empty();
+         // add new graph
+         var graph = new Rickshaw.Graph( {
+             element: document.querySelector("#graph"),
+             width: 600,
+             height: 400,
+             series: [{
+                 color: 'steelblue',
+                 data: data
+             }]
+         });
+         // set the axes
+         var axes = new Rickshaw.Graph.Axis.X({
+             graph: graph,
+             orientation: 'top'
+         });
+
+         var y_axis = new Rickshaw.Graph.Axis.Y({
+             graph: graph,
+             orientation: 'left',
+             tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
+             element: document.getElementById('y_axis')
+         });
+         graph.render();
+     };
+
+     function plot_model(well, dye){
+         var id = 1; // TODO: vary based on the opened page
+         var url = '/api/model/' + id + '/well/' + well + '/dye/' + dye;
+         // plot graph
+         $.getJSON(url, function(data){
+             console.log('model data:');
+             console.log(data);
+         });
+     }
+
      // initial plot
      plot_dataset(well, dye);
+     plot_model(well, dye);
 
      // on well change redraw the graph
      $('#well-input').on("change", function(){
